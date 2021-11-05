52fe1652996dd1e1ebb0bd2a46de4082f723ee15
José Erivam <erivamdev@gmail.com>
2021-11-05 17:02:51 -0300

    adding requisite 1:rocket:

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,3 +1,18 @@
-test('O primeiro link deve possuir o texto `Home`.', () => {
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderwithRouter from '../renderWithRouter';
 
-});
+// Caminho Feliz!!!
+describe('Teste se o topo da aplicação contém um conjunto fixo de links de navegação.',
+  () => {
+    test('Conter os links consecutivos com texto `Home`, About, Favorite Pokémons', () => {
+      renderwithRouter(<App />);
+      const homeLink = screen.getByText(/Home/i);
+      expect(homeLink).toBeInTheDocument();
+      const aboutLink = screen.getByText(/About/i);
+      expect(aboutLink).toBeInTheDocument();
+      const favoriteLink = screen.getByText(/Favorite Pokémons/i);
+      expect(favoriteLink).toBeInTheDocument();
+    });
+  });
